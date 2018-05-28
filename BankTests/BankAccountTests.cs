27442fa1eb7e4e984e3cc33b1805bf1d29a8dbc0
CodeOrNotToCode <CodeOrNotToCode@outlook.com>
2018-05-27 21:55:57 -0500

    Adding unit testing setup
    New testing project added and connected to the current "Bank" project.

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
